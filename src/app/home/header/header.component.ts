d7ba834a6579b7cd6a3b662869abeb0063d83395
Yusuke Gunji <inochideutae@gmail.com>
2021-07-04 23:17:01 +0900

    feat(header): add styles

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  addShikibetuControl() {}
 }

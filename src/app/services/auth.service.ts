0a11626bbbd24cf77edac028561dbe57180413b6
Yusuke Gunji <inochideutae@gmail.com>
2021-07-06 01:33:37 +0900

    feat(services): add user$

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { Observable, of } from 'rxjs';
+import { User } from '../interfaces/user';
+import { switchMap } from 'rxjs/operators';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   isProcessing: boolean = false;
+  user$: Observable<User> = this.afAuth.authState.pipe(
+    switchMap((afUser) => {
+      if (afUser) {
+        return this.userService.getUserData(afUser.uid);
+      } else {
+        return of(null);
+      }
+    })
+  );
 
   constructor(
     private afAuth: AngularFireAuth,
     private snackBar: MatSnackBar,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) {}
 
   async googleLogin(): Promise<void> {
@@ -31,5 +46,16 @@ export class AuthService {
         console.error(error);
       })
       .finally(() => (this.isProcessing = false));
+  }
+
+  async logout(): Promise<void> {
+    this.afAuth
+      .signOut()
+      .then(() => {
+        this.router.navigateByUrl('/welcome');
+      })
+      .then(() => {
+        this.snackBar.open('ログアウトしました');
+      });
   }
 }

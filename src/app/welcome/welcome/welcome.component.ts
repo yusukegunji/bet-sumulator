f38792b0cb621d59f2748536485ac8d9b411cb1b
Yusuke Gunji <inochideutae@gmail.com>
2021-07-05 10:38:13 +0900

    feat(app): add welcome

---
+++ src/app/welcome/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+})
+export class WelcomeComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {}
+}

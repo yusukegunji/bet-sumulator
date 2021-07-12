4d67a3e15fa197d69df1e201c25a28060cf6a81e
yusukegunji <inochideutae@gmail.com>
2021-07-12 21:46:18 +0900

    feat(tansho): fix formArray

--- src/app/home/tansho/tansho.component.ts
+++ src/app/home/tansho/tansho.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Rank } from 'src/app/interfaces/rank';
 import { BetService } from 'src/app/services/bet.service';
 import Ranks from '../../rank.json';
@@ -11,36 +11,47 @@ import Ranks from '../../rank.json';
 })
 export class TanshoComponent implements OnInit {
   ranks: Rank[] = Ranks;
+  betMoneyGroup: FormGroup;
 
   get betForms(): FormArray {
     return this.betMoneyGroup.get('betForms') as FormArray;
   }
 
-  get bet(): FormArray {
-    return this.betForms.get('bet') as FormArray;
+  // get betForm(): FormControl {
+  //   return this.betForms.get('betForm') as FormControl;
+  // }
+
+  constructor(private betService: BetService, private fb: FormBuilder) {}
+
+  buildBetForms() {
+    return this.fb.group({
+      betForm: [''],
+    });
   }
 
-  betMoneyGroup: FormGroup = new FormGroup({
-    betForms: new FormArray([new FormControl()]),
-  });
-
-  constructor(private betService: BetService) {}
+  addBetforms() {
+    this.betForms.push(this.buildBetForms());
+  }
 
   ngOnInit(): void {
-    this.betMoneyGroup.valueChanges.subscribe((val) => {
-      console.log(val.betForms);
+    this.betMoneyGroup = this.fb.group({
+      betForms: this.fb.array([]),
     });
 
+    this.addBetforms();
     this.betMoneyGroup.valueChanges.subscribe((val) => {
-      let sum = 0;
-      for (let key in val.betForms) {
-        if (val.hasOwnProperty(key)) {
-          sum = val[key] + sum;
-        }
-        console.log(sum);
-        this.betService.totalBet = sum;
-      }
+      console.log(val);
     });
+    // this.betMoneyGroup.valueChanges.subscribe((val) => {
+    //   let sum = 0;
+    //   for (let key in val.betForms) {
+    //     if (val.hasOwnProperty(key)) {
+    //       sum = val[key] + sum;
+    //     }
+    //     console.log(sum);
+    //     this.betService.totalBet = sum;
+    //   }
+    // });
   }
 
   betMoney(): void {

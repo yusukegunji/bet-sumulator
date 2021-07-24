8801c80f478c06759fc88e655b6c6fad219eb238
Yusuke Gunji <inochideutae@gmail.com>
2021-07-24 16:47:39 +0900

    feat(envitoments): add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB_IP5ipZGucCTsMsoNg-iKPoBCh52ZNoQ',
+    authDomain: 'bet-simulator-44c4b.firebaseapp.com',
+    projectId: 'bet-simulator-44c4b',
+    storageBucket: 'bet-simulator-44c4b.appspot.com',
+    messagingSenderId: '279464798355',
+    appId: '1:279464798355:web:40e6d6b3145ba6027c4566',
+    measurementId: 'G-YD0KES5Z4N',
+  },
 };

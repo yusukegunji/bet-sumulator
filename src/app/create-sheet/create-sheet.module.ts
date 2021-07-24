f57a5525dd9c382e73c70693622a3beb9ec483b9
Yusuke Gunji <inochideutae@gmail.com>
2021-07-25 06:41:36 +0900

    feat(create): add styles

--- src/app/create-sheet/create-sheet.module.ts
+++ src/app/create-sheet/create-sheet.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CreateSheetRoutingModule } from './create-sheet-routing.module';
 import { CreateSheetComponent } from './create-sheet/create-sheet.component';
 import { SelectorsModule } from '../selectors/selectors.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [CreateSheetComponent],
-  imports: [CommonModule, CreateSheetRoutingModule, SelectorsModule],
+  imports: [
+    CommonModule,
+    CreateSheetRoutingModule,
+    SelectorsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class CreateSheetModule {}

3f03aabc750d8089905f42bd55fcd99ee44705e5
Yusuke Gunji <inochideutae@gmail.com>
2021-07-26 01:36:04 +0900

    feat(create-sheet): add forms

--- src/app/create-sheet/create-sheet.module.ts
+++ src/app/create-sheet/create-sheet.module.ts
@@ -6,6 +6,10 @@ import { CreateSheetComponent } from './create-sheet/create-sheet.component';
 import { SelectorsModule } from '../selectors/selectors.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [CreateSheetComponent],
@@ -15,6 +19,11 @@ import { MatButtonModule } from '@angular/material/button';
     SelectorsModule,
     MatIconModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
   ],
 })
 export class CreateSheetModule {}
--- src/app/create-sheet/create-sheet/create-sheet.component.ts
+++ src/app/create-sheet/create-sheet/create-sheet.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import YosokaList from '../../yosoka.json';
+import JraList from '../../jra.json';
+import NarList from '../../nar.json';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { eitherRequiredValidator } from 'src/app/validators/eitherRequired.validator';
 @Component({
   selector: 'app-create-sheet',
   templateUrl: './create-sheet.component.html',
-  styleUrls: ['./create-sheet.component.scss']
+  styleUrls: ['./create-sheet.component.scss'],
 })
 export class CreateSheetComponent implements OnInit {
+  yosokaList = YosokaList;
+  jra = JraList;
+  nar = NarList;
+  isProcessing: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  forms = this.fb.group(
+    {
+      yosokaControl: ['', [Validators.required]],
+      jraControl: [''],
+      narControl: [''],
+    },
+    {
+      validator: eitherRequiredValidator('jraControl', 'narControl'),
+    }
+  );
+  get f() {
+    return this.forms.controls;
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  createSheet(): void {
+    console.log(this.forms.value);
+  }
 }

26579f894ff5f9f96e63badc6a57875661519c09
Yusuke Gunji <inochideutae@gmail.com>
2021-07-26 23:56:11 +0900

    feat(welcome): add app-signin

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -4,9 +4,23 @@ import { CommonModule } from '@angular/common';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { MatButtonModule } from '@angular/material/button';
+import { SigninComponent } from './signin/signin.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [WelcomeComponent],
-  imports: [CommonModule, WelcomeRoutingModule, MatButtonModule],
+  declarations: [WelcomeComponent, SigninComponent],
+  imports: [
+    CommonModule,
+    WelcomeRoutingModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class WelcomeModule {}

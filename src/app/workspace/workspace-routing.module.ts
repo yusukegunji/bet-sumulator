f4a89268a6ca5f451d254cfd57c5cb1998bbf10a
Yusuke Gunji <inochideutae@gmail.com>
2021-07-27 04:24:21 +0900

    feat(workspace): add sheet$

--- src/app/workspace/workspace-routing.module.ts
+++ src/app/workspace/workspace-routing.module.ts
@@ -4,7 +4,7 @@ import { WorkspaceComponent } from './workspace/workspace.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: ':id',
     component: WorkspaceComponent,
   },
 ];
--- src/app/workspace/workspace/workspace.component.ts
+++ src/app/workspace/workspace/workspace.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import deepmerge from 'deepmerge';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Jo } from 'src/app/interfaces/jo';
+import { Sheet } from 'src/app/interfaces/sheet';
+import { Yosoka } from 'src/app/interfaces/yosoka';
 import { CheckService } from 'src/app/services/check.service';
+import { SheetService } from 'src/app/services/sheet.service';
 import { UiService } from 'src/app/services/ui.service';
+import JraList from '../../jra.json';
+import NarList from '../../nar.json';
+import YosokaList from '../../yosoka.json';
 
 @Component({
   selector: 'app-workspace',
@@ -10,11 +20,30 @@ import { UiService } from 'src/app/services/ui.service';
 })
 export class WorkspaceComponent implements OnInit {
   result$: Observable<number[][]> = this.checkService.result$;
+  sheet$: Observable<Sheet> = this.route.paramMap.pipe(
+    switchMap((param) => {
+      const sheetId = param.get('id');
+      return this.sheetService.getSheet(sheetId);
+    })
+  );
+  yosokaList: Yosoka[] = YosokaList;
+  joList = deepmerge(JraList, NarList);
+  sheet: Sheet;
+  yosoka: Yosoka;
+  jo: Jo;
 
   constructor(
     public uiService: UiService,
-    private checkService: CheckService
+    private checkService: CheckService,
+    private sheetService: SheetService,
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.sheet$.subscribe((sheet) => {
+      this.sheet = sheet;
+      this.yosoka = this.yosokaList.find((data) => data.id === sheet.yosokaId);
+      this.jo = this.joList.find((jo) => jo.value === sheet.joId);
+    });
+  }
 }

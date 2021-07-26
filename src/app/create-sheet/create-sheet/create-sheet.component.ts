c47c419907724f7eaf3f2889af0838aa5a6c772d
Yusuke Gunji <inochideutae@gmail.com>
2021-07-27 04:22:57 +0900

    fix(create-sheet); fix createSheet

--- src/app/create-sheet/create-sheet/create-sheet.component.ts
+++ src/app/create-sheet/create-sheet/create-sheet.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import YosokaList from '../../yosoka.json';
-import JraList from '../../jra.json';
-import NarList from '../../nar.json';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { eitherRequiredValidator } from 'src/app/validators/eitherRequired.validator';
-import { SheetService } from 'src/app/services/sheet.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import firebase from 'firebase/app';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/interfaces/user';
+import { Yosoka } from 'src/app/interfaces/yosoka';
 import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { SheetService } from 'src/app/services/sheet.service';
+import { eitherRequiredValidator } from 'src/app/validators/eitherRequired.validator';
+import JraList from '../../jra.json';
+import NarList from '../../nar.json';
+import YosokaList from '../../yosoka.json';
 @Component({
   selector: 'app-create-sheet',
   templateUrl: './create-sheet.component.html',
   styleUrls: ['./create-sheet.component.scss'],
 })
 export class CreateSheetComponent implements OnInit {
-  yosokaList = YosokaList;
+  yosokaList: Yosoka[] = YosokaList;
   jra = JraList;
   nar = NarList;
   isProcessing: boolean;
@@ -47,7 +47,16 @@ export class CreateSheetComponent implements OnInit {
 
   createSheet(uid: string): void {
     console.log(this.forms.value);
+    const formData = this.forms.value;
+    const sheetData = {
+      yosokaId: formData.yosokaControl,
+      joId: formData.jraControl || formData.narControl,
+      uid: uid,
+      createdAt: firebase.firestore.Timestamp.now(),
+      updatedAt: firebase.firestore.Timestamp.now(),
+    };
+    console.log(sheetData);
 
-    this.sheetService.createSheet(this.forms.value, uid);
+    this.sheetService.createSheet(sheetData);
   }
 }

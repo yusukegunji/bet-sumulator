3b885f9b87c54b4c5bb914db3fab5562e8e71f69
Yusuke Gunji <inochideutae@gmail.com>
2021-07-28 03:19:46 +0900

    feat(side-nav): add navigate, close nav

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UiService } from '../services/ui.service';
 
 @Component({
@@ -7,11 +8,15 @@ import { UiService } from '../services/ui.service';
   styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
-  constructor(private uiService: UiService) {}
+  constructor(private uiService: UiService, private router: Router) {}
 
   ngOnInit(): void {}
 
   toggleNav() {
     this.uiService.isSidenavOpen = !this.uiService.isSidenavOpen;
+  }
+
+  navigate(param: string) {
+    this.router.navigateByUrl(param).finally(() => this.toggleNav());
   }
 }

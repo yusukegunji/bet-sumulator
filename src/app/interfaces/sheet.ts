6ca6fd3f3fe0c38b269c10518337066cad679cad
Yusuke Gunji <inochideutae@gmail.com>
2021-07-28 23:39:55 +0900

    feat(sheet.ts): add WithUser, Jo, Yosoka

--- src/app/interfaces/sheet.ts
+++ src/app/interfaces/sheet.ts
@@ -1,14 +1,35 @@
+import { Jo } from './jo';
+import { User } from './user';
 import { Yosoka } from './yosoka';
 
 export interface Sheet {
   id: string;
   yosokaId: string;
   joId: string;
-  uid: string;
+  createdUid: string;
+  updatedUid: string;
   createdAt: firebase.default.firestore.Timestamp;
   updatedAt: firebase.default.firestore.Timestamp;
 }
 
 export interface SheetWithYosoka extends Sheet {
   yosoka: Yosoka;
+}
+
+export interface SheetWithUser extends Sheet {
+  createdUser: User;
+  updatedUser: User;
+}
+
+export interface SheetWithUserWithYosoka extends Sheet {
+  createdUser: User;
+  updatedUser: User;
+  yosoka: Yosoka;
+}
+
+export interface SheetWithUserWithYosokaWithJo extends Sheet {
+  createdUser: User;
+  updatedUser: User;
+  yosoka: Yosoka;
+  jo: Jo;
 }

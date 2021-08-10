c546977e90a4099bc6e758be308d439ba8838207
Yusuke Gunji <inochideutae@gmail.com>
2021-08-10 09:39:00 +0900

    feat(bet.service): add win, dividend, recovery

--- src/app/services/bet.service.ts
+++ src/app/services/bet.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Race } from '../interfaces/race';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +8,13 @@ export class BetService {
   totalBet: number = 0;
   betStockArr: number[] = [];
 
+  betNum: number = 0;
+  winningRate: number = 0;
+  winningRacesArr: Race[] = [];
+
+  totalAvgDividendArr: number[] = [];
+  totalDividendArr: number[] = [];
+  totalRecoveryNum: number = 0;
+  recoveryRate: number = 0;
   constructor() {}
 }

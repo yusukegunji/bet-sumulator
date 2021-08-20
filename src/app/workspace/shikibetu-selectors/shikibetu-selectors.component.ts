084e050785e2aaa6bc8ba36d5e7427a2c9f10ae3
Yusuke Gunji <inochideutae@gmail.com>
2021-08-20 22:57:56 +0900

    feat(shikibetu-select): add @input

--- src/app/workspace/shikibetu-selectors/shikibetu-selectors.component.ts
+++ src/app/workspace/shikibetu-selectors/shikibetu-selectors.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Sheet } from 'src/app/interfaces/sheet';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-shikibetu-selectors',
   templateUrl: './shikibetu-selectors.component.html',
-  styleUrls: ['./shikibetu-selectors.component.scss']
+  styleUrls: ['./shikibetu-selectors.component.scss'],
 })
 export class ShikibetuSelectorsComponent implements OnInit {
+  @Input() sheet: Sheet;
+  @Input() user: User;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

4ddb8696bf2f7149bc0327152483c4019cdcdc03
Yusuke Gunji <inochideutae@gmail.com>
2021-08-20 22:59:40 +0900

    feat(select): add setShikibetu

--- src/app/selectors/select/select.component.ts
+++ src/app/selectors/select/select.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Sheet } from 'src/app/interfaces/sheet';
+import { User } from 'src/app/interfaces/user';
+import { SheetService } from 'src/app/services/sheet.service';
 import { UiService } from 'src/app/services/ui.service';
 import ShikibetuList from '../../shikibetu.json';
 
@@ -10,6 +13,8 @@ import ShikibetuList from '../../shikibetu.json';
 })
 export class SelectComponent implements OnInit {
   @Input() type: string = 'yosoka' || 'jra' || 'nar' || 'shikibetu';
+  @Input() sheet: Sheet;
+  @Input() user: User;
   shikibetuList = ShikibetuList;
   formGroup: FormGroup;
 
@@ -23,7 +28,11 @@ export class SelectComponent implements OnInit {
     return this.formGroup.get('shikibetuControls') as FormArray;
   }
 
-  constructor(public uiService: UiService, private fb: FormBuilder) {}
+  constructor(
+    public uiService: UiService,
+    private fb: FormBuilder,
+    private sheetService: SheetService
+  ) {}
 
   ngOnInit(): void {
     this.formGroup = this.createForm();
@@ -39,6 +48,7 @@ export class SelectComponent implements OnInit {
     const val = this.shikibetuControls.controls[i].value.shikibetuControl;
     this.uiService.shikibetuGroup[i] = val;
     this.uiService.shikibetu = val;
+    this.sheetService.setShikibetu(this.sheet.id, val, this.user.uid);
 
     if (val === 'umaren' || val === 'umatan' || val === 'wide') {
       this.uiService.isTwoHorseOptionNavOpen = true;
